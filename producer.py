07fd93ba7ff2cfa0201d61b5e8cd58c918b9f72e
Derek Sarmiento Loeber <130619756+DereKk8@users.noreply.github.com>
2025-05-14 16:39:08 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -30,7 +30,7 @@ def main():
             logging.debug("Got weather: %s", weather)
             producer.produce(
                 topic="weather_data_demo",
-                key="London",
+                key="Bogota",
                 value=json.dumps(weather),
             )
             logging.info("Produced. Sleeping...")
